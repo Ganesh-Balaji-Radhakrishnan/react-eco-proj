18d48023b43677e9592733e14d8b90b2702e2ce1
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2024-07-24 19:52:42 +0200

    implement reselect logic

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,2 +1,13 @@
+import { createSelector } from "reselect";
+
 export const getTodos = (state) => state.todos.data;
 export const getIsLoading = (state) => state.todos.isLoading;
+
+export const getCompletedTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => todo.isCompleted)
+);
+// reselect helps to save computation time since it gets re-calculated only when the inputs are changed, unlike normal functions
+// like memo-isation
+export const getInCompletedTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => !todo.isCompleted)
+);
